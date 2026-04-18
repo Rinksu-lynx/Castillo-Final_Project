7bb2ac41e2abf356606631931150cf2f2e1eb112
Rinksu-lynx <castillocaedy@gmail.com>
2026-04-18 21:43:46 +0800

    Initial Commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Contact() {
+   return (
+    <section className="my-12">
+      <h2 className="text-3xl font-semibold text-blue-600">Contact</h2>
+      <p className="text-lg mt-4">Contact deez nuts.</p>
+    </section>
+  );
+}
+
+export default Contact
---
+++ src/components/Skills.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Skills() {
+    return (
+    <section className="my-12">
+      <h2 className="text-3xl font-semibold text-blue-600">Skils</h2>
+      <p className="text-lg mt-4">Are you skilled bro?</p>
+    </section>
+  );
+}
+
+export default Skills
