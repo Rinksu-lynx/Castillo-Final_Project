bc7d61ca32fbe75ce9bc1b1b1d65bde6523e53c4
Rinksu-lynx <castillocaedy@gmail.com>
2026-04-18 21:43:46 +0800

    Initial Commit

---
+++ src/components/Education.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Education() {
+    return (
+    <section className="my-12">
+      <h2 className="text-3xl font-semibold text-blue-600">Education</h2>
+      <p className="text-lg mt-4">Education is my passion.</p>
+    </section>
+  );
+}
+
+export default Education
---
+++ src/components/Experience.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Experience() {
+   return (
+    <section className="my-12">
+      <h2 className="text-3xl font-semibold text-blue-600">Experience</h2>
+      <p className="text-lg mt-4">The full sisig experience.</p>
+    </section>
+  );
+}
+
+export default Experience
